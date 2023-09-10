2bf3dd6775b60c4bada8111f801aa7ea51b20399
Divy611 <divy.parikh@hotmail.com>
2023-09-11 04:43:47 +0530

    Contact Us Page added Modifications in the home Page

---
+++ src/app/app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.getElementById('menuToggle');
+    const sidebar = document.getElementById('sidebar');
+    menuToggle.addEventListener('click', function () {
+        sidebar.classList.toggle('show');
+    });
+});
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Aos from 'aos';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
-
+export class ContactComponent implements OnInit {
+  ngOnInit(): void {
+    Aos.init({
+      once: false,
+      //offset: 120,
+    });
+    document.querySelectorAll('img').forEach((img) => img.addEventListener('load', () => Aos.refresh()));
+  }
 }
